d8b4f09f345bbe2d4f584c9b8d5647394e334d42
anassMQ <maqbour2006@gmail.com>
2026-09-09 22:00:36 +0100

    Fix vercel build

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -12,7 +12,36 @@ const Routes = () => {
   return (
     <Switch>
       <Route
-        path={[`/${Config.LOGIN_PAGE}`, `/${Config.HOME_PAGE}/${Config.LOGIN_PAGE}`]}
+        path={[
+          `/${Config.LOGIN_PAGE}`,
+          `/${Config.HOME_PAGE}/${Config.LOGIN_PAGE}`,
+        ]}
         exact
         component={Login}
       />
+
+      <Route
+        path={`/${Config.HOME_PAGE}/:category/search/:keyword`}
+        component={Catalog}
+      />
+
+      <Route
+        path={`/${Config.HOME_PAGE}/:category/:id`}
+        component={Detail}
+      />
+
+      <Route
+        path={`/${Config.HOME_PAGE}/:category`}
+        component={Catalog}
+      />
+
+      <Route
+        path={`/${Config.HOME_PAGE}`}
+        exact
+        component={Home}
+      />
+    </Switch>
+  );
+};
+
+export default Routes;
