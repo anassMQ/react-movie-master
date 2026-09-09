089524f8990d15abeb406abcef74e0f38b019ea8
anassMQ <maqbour2006@gmail.com>
2026-09-09 21:57:13 +0100

    Fix vercel build
    Removed unused route definitions to simplify routing.

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -7,11 +7,7 @@ import Detail from "../pages/detail/Detail";
 import Login from "../pages/Login";
 
 import * as Config from "../constants/Config";
-<Route
-  path={[`/${Config.LOGIN_PAGE}`, `/${Config.HOME_PAGE}/${Config.LOGIN_PAGE}`]}
-  exact
-  component={Login}
-/>
+
 const Routes = () => {
   return (
     <Switch>
@@ -20,15 +16,3 @@ const Routes = () => {
         exact
         component={Login}
       />
-      <Route
-        path={`/${Config.HOME_PAGE}/:category/search/:keyword`}
-        component={Catalog}
-      />
-      <Route path={`/${Config.HOME_PAGE}/:category/:id`} component={Detail} />
-      <Route path={`/${Config.HOME_PAGE}/:category`} component={Catalog} />
-      <Route path={`/${Config.HOME_PAGE}`} exact component={Home} />
-    </Switch>
-  );
-};
-
-export default Routes;
